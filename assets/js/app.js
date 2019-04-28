8ebd6c5c8982046d8c1ff934b646d655be79031a
connor scoma <connorscoma@connors-MacBook-Pro.local>
2019-04-28 13:01:47 -0500

    adding stuff to DOM

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,13 +3,20 @@ var question = [
     "At Dartmouth College in 1964 John Kemeny and Thomas Kurtz invented ____?", // Basic
     "Who invented C++ ?", // Bjarne Stroustrup
     "Which of these is not a functional programming language", // Fortran
-    "A true or false statement put into code that the programmer expects to always be true is an ____", // assertion
+    "A true or false statement put into code that the programmer expects to always be true is an ____. ", // assertion
     "A section of code that responds to a particular interaction of the user with a gui control is called a ____?", // event handler
 ];
 //Create Score
 var score = 0;
 // Create User Answer
 
+$("#question").text(question[0])
+
+// $('#question').text(function() {
+//     for(var i = 0; i <= question.length; i++) {
+    
+//     }
+// });
 
 //Create function
     //Create Loop
