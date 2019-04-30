15ea16af0bc7f15fb364ca9016de57278f320b1b
connor scoma <connorscoma@connors-MacBook-Pro.local>
2019-04-30 11:22:37 -0500

    adding all logic

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,23 +1,106 @@
 //Create Question
-var question = [
-    "At Dartmouth College in 1964 John Kemeny and Thomas Kurtz invented ____?", // Basic
-    "Who invented C++ ?", // Bjarne Stroustrup
-    "Which of these is not a functional programming language", // Fortran
-    "A true or false statement put into code that the programmer expects to always be true is an ____. ", // assertion
-    "A section of code that responds to a particular interaction of the user with a gui control is called a ____?", // event handler
+var questions = [
+    {
+    question : "At Dartmouth College in 1964 John Kemeny and Thomas Kurtz invented ____?", // Basic
+    choiceA : "Basic",
+    choiceB : "Algol",
+    choiceC: "Fortran",
+    choiceD: "C",
+    correct : "a",
+    },
+    {
+    question : "Who invented C++ ?", // Bjarne Stroustrup
+    choiceA : "Larry Wall",
+    choiceB : "Anders Hejlsberg",
+    choiceC : "Bjarne Stroustrup",
+    choiceD : "John Backus",
+    correct : "c",
+    },
+    {
+   question : "Which of these is not a functional programming language", // Fortran
+   choiceA : "Erlang",
+   choiceB : "Fortran",
+   choiceC : "APL",
+   choiceD : "Miranda",
+   correct : "b",
+
+    },
+    {
+   question : "A true or false statement put into code that the programmer expects to always be true is an ____. ", // assertion
+   choiceA : "Exception",
+   choiceB : "Expression",
+   choiceC : "Harness",
+   choiceD : "Assertion",
+   correct : "d"
+    },
+    {
+   question : "A section of code that responds to a particular interaction of the user with a gui control is called a ____?", // event handler
+   choiceA : "Event Handler",
+   choiceB : "Control Structure",
+   choiceC : "Exception Handler",
+   choiceD : "Dispatch Function",
+   correct : "a"
+    },
 ];
-//Create Score
-var score = 0;
-// Create User Answer
 
-$("#question").text(question[0])
+var questionTimer = 10;
+var startTimer = 0
 
-// $('#question').text(function() {
-//     for(var i = 0; i <= question.length; i++) {
-    
-//     }
-// });
+var question = document.getElementById('question');
+
+var userScore = 0;
+var wrongAnswer = 0;
+
+var timer = document.getElementById('timer');
+
+const choiceA = document.getElementById('a');
+const choiceB = document.getElementById('b');
+const choiceC = document.getElementById('c');
+const choiceD = document.getElementById('d');
+
+const lastQuestionIndex = questions.length -1;
+let questionIndex = 0;
+
 
 //Create function
-    //Create Loop
-        //Create if 
+function questionsIndex() {
+    var q = questions[questionIndex];
+    question.innerHTML = "</p>" + q.question + "<p>"
+    choiceA.innerHTML = q.choiceA;
+    choiceB.innerHTML = q.choiceB;
+    choiceC.innerHTML = q.choiceC;
+    choiceD.innerHTML = q.choiceD;
+};
+
+
+function counter() {
+    if(startTimer <= questionTimer ){
+        timer.innerHTML = startTimer;
+        startTimer++;
+    } else {
+        startTimer = 0;
+            if(questionIndex < lastQuestionIndex){
+                questionIndex++;
+                questionsIndex();
+            }
+    }
+};
+
+
+
+function checkanswer(answer) {
+    if(questions[questionIndex].correct == answer){
+        userScore++;
+    } else {
+        wrongAnswer++;
+    }
+    if (questionIndex < lastQuestionIndex) {
+        startTimer = 0;
+        questionIndex++;
+        questionsIndex();
+    }
+}
+
+checkanswer();
+
+setInterval(counter, 1000)
